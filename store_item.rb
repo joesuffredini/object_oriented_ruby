af63abdc1aefadc6d94ec0bf080b9a290c35c709
Joe Suffredini <gsuffred@gmail.com>
2021-02-17 14:33:05 -0500

    updates with store items included

--- store_item.rb
+++ store_item.rb
@@ -1,1 +1,9 @@
 #Store Inventory
+store_fruit = {}
+store_meat = {}
+store_beverages = {}
+store_fruit = { :type => "Oranges", :color => "orange", :price => 2 }
+store_meat = { :type => "Hamburger", :color => "red", :price => 4 }
+store_beverages = { :type => "Coke", :color => "black", :price => 2 }
+puts "The store has only this fruit #{store_fruit[:type]} and it costs #{store_fruit[:price]} "
+puts "The store has type of meat #{store_meat[:type]} and it costs #{store_meat[:price]} "
