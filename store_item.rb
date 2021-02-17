ed51499bac21097836216c70fe2e2f8814d2dca7
Joe Suffredini <gsuffred@gmail.com>
2021-02-17 15:26:12 -0500

    made some puts output statements

--- store_item.rb
+++ store_item.rb
@@ -5,8 +5,8 @@ store_beverages = {}
 store_fruit = { :type => "Oranges", :color => "orange", :price => 2 }
 store_meat = { :type => "Hamburger", :color => "red", :price => 4 }
 store_beverages = { :type => "Coke", :color => "black", :price => 2 }
-puts "The store has only this fruit #{store_fruit[:type]} and it costs #{store_fruit[:price]} "
-puts "The store has type of meat #{store_meat[:type]} and it costs #{store_meat[:price]} "
+puts "The store only has this fruit: #{store_fruit[:type]} and it costs: $#{store_fruit[:price]} "
+puts "The store only has type of meat: #{store_meat[:type]} and it costs: $#{store_meat[:price]} "
 
 class Store
   attr_reader :type, :color, :price
@@ -18,7 +18,7 @@ class Store
   end
 
   def print_info
-    puts "The store has #{@type} at this price $#{@price}"
+    puts "The store has #{@type} at this price: $#{@price}"
   end
 end
 
